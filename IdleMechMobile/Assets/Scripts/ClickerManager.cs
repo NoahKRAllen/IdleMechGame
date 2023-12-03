2b0ad1d643227f00aed099555436d1a8d6d2c465
Noah Allen <noahkrallen@gmail.com>
2023-12-03 10:25:30 -0800

    Progress being made on multi-screen UI and decoupling code

---
+++ IdleMechMobile/Assets/Scripts/ClickerManager.cs
@@ -0,0 +1,52 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickerManager : MonoBehaviour
+{
+    [SerializeField]
+    private Button moneyUpButton;
+    [SerializeField]
+    private TextMeshProUGUI moneyUpButtonText;
+    private MoneyController _moneyController;
+
+    private int _moneyIntIncrease = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!moneyUpButton)
+        {
+            Debug.Log("Missing money increase button");
+        }
+
+        if (!moneyUpButtonText)
+        {
+            Debug.Log("Missing money increase button text");
+        }
+
+        if (!_moneyController)
+        {
+            _moneyController = GetComponent<MoneyController>();
+        }
+    }
+
+    public void CallIncreaseMoney(float amountToAdd)
+    {
+        if (amountToAdd > 0)
+        {
+            _moneyController.IncreaseMoney(amountToAdd);
+        }
+    }
+    
+
+    public void AddClickMoney()
+    {
+        _moneyController.IncreaseMoney(_moneyIntIncrease);
+    }
+    public void IncreaseClickValue()
+    {
+        _moneyIntIncrease++;
+        moneyUpButtonText.text = "+" + _moneyIntIncrease + " Money";
+    }
+}
---
+++ IdleMechMobile/Assets/Scripts/ScreenManager.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenManager : MonoBehaviour
+{
+    private GameObject _currentlyActiveScreen;
+
+    private void Start()
+    {
+        _currentlyActiveScreen = GetComponent<
+    }
+
+    public void SwapScreenTo(GameObject screenToSwapTo)
+    {
+        
+    }
+}
